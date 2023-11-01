6d0131bdf8e70fe007326c12105efed9b1a44df1
rubenpelalv <rpellial7@alumnes.ub.edu>
2023-11-01 21:09:40 +0100

    US19 estructura, falta añadir boton para ir al Inicio arriba a la izquierda, recoger los datos actuales de la bdd, y guardar la foto de perfil en la bdd

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Inicio from '@/components/Inicio'
 import Login from '@/components/Login'
+import Settings from '@/components/Settings'
 
 Vue.use(Router)
 
@@ -16,6 +17,12 @@ export default new Router({
       path: '/inicio',
       name: 'Inicio',
       component: Inicio
+    },
+    {
+      path: '/settings',
+      name: 'Settings',
+      component: Settings
     }
+
   ]
 })
